4446423e19adfc01ee1010101c32cb1d8963f240
2694942998@qq.com <2694942998@qq.com>
2019-08-27 16:52:54 +0800

    1

---
+++ num.js
@@ -0,0 +1,15 @@
+function Nums() {
+     var nums=''
+    var chars = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
+      'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e',
+      'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
+    ];
+
+    for (var i = 0; i < 32; i++) {
+      var id = parseInt(Math.random() * 61);
+      nums += chars[id];
+    }
+    return nums;
+  }
+
+  console.log(Nums())
